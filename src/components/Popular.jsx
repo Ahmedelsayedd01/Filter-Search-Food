a0b1b1d23740a3e66594c6a5493ea3e66459e92e
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-22 16:26:28 +0200

    New Update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -18,15 +21,66 @@ function Popular() {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <div className="my-[4rem] mx-0">
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <div className="relative min-w-[25rem] overflow-hidden">
+                  <p className="absolute z-10 left-1/2 bottom-1/2 -translate-x-[20%] translate-y-0 text-white w-full">
+                    {recipe.title}
+                  </p>
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                    className="absolute rounded-[2rem] w-full object-cover left-0"
+                  />
+                </div>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </div>
     </div>
   );
 }
+
+// const Wrapper = styled.div`
+//   margin: 4rem 0rem;
+// `;
+// const Card = styled.div`
+//   min-width: 25rem;
+//   border-radius: 2rem;
+//   overflow: hidden;
+//   position: relative;
+
+//   img {
+//     border-radius: 2rem;
+//     position: absolute;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     object-fit: cover;
+//   }
+//   p {
+//     position: absolute;
+//     z-index:10;
+//     left:50%;
+//     bottom:0%;
+//     transform: translate(-50%, 0%);
+//     color: white;
+//     width:100%;
+//   }
+// `;
 
 export default Popular;

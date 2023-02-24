d99c43eac340f12dbf6bf97f1881df6541efdbb6
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-24 21:53:16 +0200

    New Update

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 function Veggie() {
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+      // console.log(data.recipes);
+    }
+  };
   return (
-    <div>Veggie</div>
-  )
+    <div>
+      <div className="my-[4rem] mx-0">
+        <h3>Our Vegetarian Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {veggie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <div className="relative flex items-center justify-center rounded-[2rem] min-w-[24rem] overflow-hidden">
+                  <p className="absolute bottom-5 z-20 text-white w-full text-center font-semibold	text-base h-2/5 flex justify-center items-center">
+                    {recipe.title}
+                  </p>
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                    className="rounded-[2rem] w-full h-full object-cover"
+                  />
+                  <div className="absolute z-10 w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.5)] "></div>
+                </div>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </div>
+    </div>
+  );
 }
 
-export default Veggie
+export default Veggie;

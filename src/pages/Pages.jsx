9ed034da0b49baf30369664e5e4b39fc9b5639bf
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-27 00:10:44 +0200

    New Update

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import Home from "./Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Category from "../components/Category";
 function Pages() {

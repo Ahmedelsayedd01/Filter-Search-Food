33983da759a04d8c88a76ddc54e448af3edde1e1
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-26 23:55:45 +0200

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Category from "./components/Category";
+// import Category from "./components/Category";
 import Pages from "./pages/Pages";
 function App() {
   return (
     <div>
-      <Category />
+      
       <Pages />
     </div>
   );

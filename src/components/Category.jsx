03cf26c442449027cea222a7897524e5ba468a6c
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-26 23:57:15 +0200

    New Update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,25 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function Category() {
   return (
     <div className="flex justify-center my-2 mx-0">
-      <Link to={"/cuisine"}>
+      <NavLink to={"/cuisine"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </Link>
-      <div>
+      </NavLink>
+      <NavLink to={}>
         <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
+      </NavLink>
     </div>
   );
 }

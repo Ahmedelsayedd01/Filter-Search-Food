eb97f4833ff9d30b8c19007b1771b5834e03937e
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-27 22:50:04 +0200

    New Update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,21 +4,33 @@ import { NavLink } from "react-router-dom";
 function Category() {
   return (
     <div className="flex justify-center my-2 mx-0">
-      <NavLink to={"/cuisine/Italian"} className="flex flex-col justify-center items-center rounded-[50%] mr-8 text-d">
-        <FaPizzaSlice />
-        <h4>Italian</h4>
+      <NavLink
+        to={"/cuisine/Italian"}
+        className="flex flex-col justify-center items-center rounded-[50%] mr-8 bg-gradient-to-r from-[#494949] to-[#313131] w-24 h-24 cursor-pointer transform scale-90 focus:gradient"
+      >
+        <FaPizzaSlice className="text-white text-2xl" />
+        <h4 className="text-white text-sm">Italian</h4>
       </NavLink>
-      <NavLink to={"/cuisine/American"}>
-        <FaHamburger />
-        <h4>American</h4>
+      <NavLink
+        to={"/cuisine/American"}
+        className="flex flex-col justify-center items-center rounded-[50%] mr-8 bg-gradient-to-r from-[#494949] to-[#313131] w-24 h-24 cursor-pointer transform scale-90 focus:gradient"
+      >
+        <FaHamburger className="text-white text-2xl" />
+        <h4 className="text-white text-sm">American</h4>
       </NavLink>
-      <NavLink to={"/cuisine/Thai"}>
-        <GiNoodles />
-        <h4>Thai</h4>
+      <NavLink
+        to={"/cuisine/Thai"}
+        className="flex flex-col justify-center items-center rounded-[50%] mr-8 bg-gradient-to-r from-[#494949] to-[#313131] w-24 h-24 cursor-pointer transform scale-90 focus:gradient"
+      >
+        <GiNoodles className="text-white text-2xl" />
+        <h4 className="text-white text-sm">Thai</h4>
       </NavLink>
-      <NavLink to={"/cuisine/Japanese"}>
-        <GiChopsticks />
-        <h4>Japanese</h4>
+      <NavLink
+        to={"/cuisine/Japanese"}
+        className="flex flex-col justify-center items-center rounded-[50%] mr-8 bg-gradient-to-r from-[#494949] to-[#313131] w-24 h-24 cursor-pointer transform scale-90 focus:gradient"
+      >
+        <GiChopsticks className="text-white text-2xl" />
+        <h4 className="text-white text-sm">Japanese</h4>
       </NavLink>
     </div>
   );

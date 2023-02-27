1e7853304760fafe87ae952f7c28a16ff29bb5af
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-27 23:14:40 +0200

    New Update

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Category from "../components/Category";
+import Search from "../components/Search";
 function Pages() {
   const route = createBrowserRouter([
     {
       path: "/",
       element: (
         <>
+          <Search />
           <Category />
           <Home />
         </>
@@ -18,6 +20,7 @@ function Pages() {
       path: "/cuisine/:type",
       element: (
         <>
+          <Search />
           <Category />
           <Cuisine />
         </>

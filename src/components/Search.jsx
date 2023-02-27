296520f09d02479d5f54f6c07a4e04c627c9aa01
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-27 23:25:14 +0200

    New Update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { FaSearch } from "react-icons/fa";
 function Search() {
   return (
-    <form className="relative w-full my-0 mx-80">
-      <div className="">
-        <input
-          type="text"
-          className="w-full border-none bg-gradient-to-r from-[#494949] to-[#313131] text-2xl text-white py-4 px-12 rounded-2xl"
-        />
-      </div>
+    <form className="relative w-full mb-2">
+      <input
+        type="text"
+        className="w-full border-none bg-gradient-to-r from-[#494949] to-[#313131] text-2xl text-white py-2 px-12 rounded-2xl"
+      />
       <FaSearch className="absolute top-2/4 left-0 transform translate-y-[-50%] translate-x-[100%] text-white" />
     </form>
   );

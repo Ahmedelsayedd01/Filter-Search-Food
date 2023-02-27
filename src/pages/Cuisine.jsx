ea8d699ae2173849b7a30a90cfe9e2537f07f185
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-27 15:09:13 +0200

    New Update

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
 function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+
   return (
-    <div>Cuisine</div>
-  )
+    <div className="grid grid-cols-4 gap-5">
+      {cuisine.map((item) => {
+        return (
+          <div className="" key={item.id}>
+            <img src={item.image} alt="" className="w-full rounded-[2rem]" />
+            <h4 className="text-center p-2">{item.title}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Cuisine
+export default Cuisine;

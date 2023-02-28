c261c5dafbff6f86b2c2e8070f4309bff388eaa7
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-28 23:05:00 +0200

    New Update

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -21,7 +21,6 @@ function Veggie() {
 
       localStorage.setItem("veggie", JSON.stringify(data.recipes));
       setVeggie(data.recipes);
-      // console.log(data.recipes);
     }
   };
   return (
@@ -41,7 +40,10 @@ function Veggie() {
           {veggie.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
-                <div className="relative flex items-center justify-center rounded-[2rem] min-h-[18rem] w-full overflow-hidden">
+                <Link
+                  to={"/recipe/" + recipe.id}
+                  className="relative flex items-center justify-center rounded-[2rem] min-h-[18rem] w-full overflow-hidden"
+                >
                   <p className="absolute bottom-5 z-20 text-white w-full text-center font-semibold	text-base h-2/5 flex justify-center items-center">
                     {recipe.title}
                   </p>
@@ -50,8 +52,8 @@ function Veggie() {
                     alt={recipe.title}
                     className="absolute rounded-[2rem] w-full h-full object-cover left-0"
                   />
-                  <div className="absolute z-10 w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.5)] "></div>
-                </div>
+                  <div className="absolute z-10 w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.3)]"></div>
+                </Link>
               </SplideSlide>
             );
           })}

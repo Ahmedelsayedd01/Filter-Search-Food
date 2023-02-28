f7e2a8d2f753b8683d4555a71456e83a446de555
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-28 21:20:31 +0200

    New UPdate

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -5,18 +5,18 @@ function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
   let params = useParams();
 
-  const getCuisine = async (name) => {
+  /*   const getCuisine = async (name) => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
     );
     const recipes = await data.json();
     setCuisine(recipes.results);
-  };
+  }; */
 
-  useEffect(() => {
-    getCuisine(params.type);
-    console.log(params.type);
-  }, [params.type]);
+  // useEffect(() => {
+  //   getCuisine(params.type);
+  //   console.log(params.type);
+  // }, [params.type]);
 
   return (
     <div className="grid grid-cols-4 gap-5">

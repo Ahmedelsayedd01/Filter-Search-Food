56e0956467f2824124ad82fd557d277d66d3c87c
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-28 22:41:02 +0200

    New Update

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Searched() {
+  const [searchedRecipes, setSearchedRecipes] = useState("");
+  const params = useParams();
+
+  const getSearced = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearced(params.search);
+  }, [params.search]);
   return <div>Searched</div>;
 }
 

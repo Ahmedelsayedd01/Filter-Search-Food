62a144c2835ff3a9247d710d4a96160b0844ed08
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-28 23:04:46 +0200

    New Update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -43,19 +43,20 @@ function Popular() {
           {popular.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
-                <div className="relative flex items-center justify-center rounded-[2rem] min-h-[18rem] w-full overflow-hidden">
-                  <Link to={"/recipe/" + recipe.id}>
-                    <p className="absolute bottom-5 z-20 text-white w-full text-center font-semibold	text-base h-2/5 flex justify-center items-center">
-                      {recipe.title}
-                    </p>
-                    <img
-                      src={recipe.image}
-                      alt={recipe.title}
-                      className="absolute rounded-[2rem] w-full h-full object-cover left-0"
-                    />
-                  </Link>
-                  <div className="absolute z-10 w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.5)] "></div>
-                </div>
+                <Link
+                  to={"/recipe/" + recipe.id}
+                  className="relative flex items-center justify-center rounded-[2rem] min-h-[18rem] w-full overflow-hidden"
+                >
+                  <p className="absolute bottom-5 z-20 text-white w-full text-center font-semibold	text-base h-2/5 flex justify-center items-center">
+                    {recipe.title}
+                  </p>
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                    className="absolute rounded-[2rem] w-full h-full object-cover left-0"
+                  />
+                  <div className="absolute z-10 w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.4)] "></div>
+                </Link>
               </SplideSlide>
             );
           })}

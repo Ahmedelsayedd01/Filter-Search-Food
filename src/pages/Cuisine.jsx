9a07e8ac351c0bb6baf80a83f455ad87f03c4379
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-28 21:12:53 +0200

    New Update

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { Link, useParams } from "react-router-dom";
+// import { motion } from "framer-motion";
+import { /* Link, */ useParams } from "react-router-dom";
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
   let params = useParams();

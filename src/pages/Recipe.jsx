2c2f85eab767925dd515c077472702c5541563d7
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-03 23:18:26 +0200

    New Update

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -43,15 +43,20 @@ function Recipe() {
             Ingredients
           </button>
         </div>
-        <div className="">
-          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
-          <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
-        </div>
-        <ul>
-          {details.extendedIngredients.map((ingredient) => (
-            <li key={ingredient.id}>{ingredient.original}</li>
-          ))}
-        </ul>
+        {activeTab === "instructions" && (
+          <div className="">
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </div>
+        )}
+
+        {activeTab === "ingredients" && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li className="" key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
